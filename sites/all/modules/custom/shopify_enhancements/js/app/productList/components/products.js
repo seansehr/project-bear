770b4e0693c0629ca46d395965ab4c50f0b597f9
xeraseth <sean@seansehr.com>
2016-09-11 16:41:09 -0700

    Updating React Masonry and removing temp fix

--- sites/all/modules/custom/shopify_enhancements/js/app/productList/components/products.js
+++ sites/all/modules/custom/shopify_enhancements/js/app/productList/components/products.js
@@ -6,8 +6,6 @@ class ProductList extends React.Component {
   render() {
     return (
       <Masonry className="products_list">
-        {/* prevents rendering issues: https://github.com/eiriklv/react-masonry-component/issues/11 */}
-        <div style={{'width': '1px', 'height': 0}} />
         {this.props.products.map(item => {
           return <Product key={item.id} data={item} />;
         })}

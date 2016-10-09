938fd19a68219819d052a9837741fc3480a239e8
xeraseth <sean@seansehr.com>
2016-10-09 14:54:20 -0700

    Hiding a tag filter if it would return all results

--- sites/all/modules/custom/shopify_enhancements/js/app/productList/selectors/tagFilter.js
+++ sites/all/modules/custom/shopify_enhancements/js/app/productList/selectors/tagFilter.js
@@ -4,15 +4,27 @@ import { productIsVisible } from './products'
 
 const tagFilter = (products, filters) => {
   let options = {}
-  products.forEach(function (product) {
+  products.forEach(function (product, index, arr) {
     if (productIsVisible(product, filters, ['tag'])) {
       let tags = product.tags.forEach(tag => {
         let color = fieldValue(tag.field_color, 'jquery_colorpicker')
+        let count = 0;
+        if (options[tag.name]) {
+          count = options[tag.name].count
+        }
         options[tag.name] = {
+          count: ++count,
           key: tag.name,
           style: color ? {color: '#' + color} : {}
         }
       })
+    }
+    if ((index + 1) >= arr.length) {
+      for (let key in options) {
+        if (options.hasOwnProperty(key) && options[key].count === arr.length) {
+          delete options[key]
+        }
+      }
     }
   });
   return objectMap(options, key => options[key]);
